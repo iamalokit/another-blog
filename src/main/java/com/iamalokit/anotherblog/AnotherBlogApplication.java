d2e27ab17a8260e98e777d404686c29c17a64dab
Alokit Jindal <iamalokit@gmail.com>
2020-12-25 21:16:26 +0530

    Initial Commit

---
+++ src/main/java/com/iamalokit/anotherblog/AnotherBlogApplication.java
@@ -0,0 +1,13 @@
+package com.iamalokit.anotherblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamalokit/anotherblog/AnotherBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamalokit.anotherblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
